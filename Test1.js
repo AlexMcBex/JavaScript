b440e78f48d10809e682fb2625a05e33548176ca
Alessio Belli <117402531+AlexMcBex@users.noreply.github.com>
2022-11-19 15:18:06 -0500

    Update Test1.js

--- Test1.js
+++ Test1.js
@@ -4,3 +4,16 @@ console.log(Math.floor(Math.random() * 50));
 
 var myName = 'Alex'
 console.log(`Hi my Name is ${myName}`);
+
+//Human Age
+const myAge = 27;
+//
+var earlyYears = 2;
+earlyYears*= 10.5;
+//
+let laterYears= myAge - 2;
+laterYears*= 4;
+console.log(earlyYears);
+console.log(laterYears);
+var myAgeInDogYears= earlyYears + laterYears;
+
