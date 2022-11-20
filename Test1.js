d3a8a0dcdef7d5d5dec283aa9e0b7efa71f22c9b
Alessio Belli <117402531+AlexMcBex@users.noreply.github.com>
2022-11-20 16:51:05 -0500

    Update Test1.js

--- Test1.js
+++ Test1.js
@@ -7,13 +7,19 @@ console.log(`Hi my Name is ${myName}`);
 
 //Human Age
 const myAge = 27;
-//
+//first 2 years of human age
 var earlyYears = 2;
+// converted in dog years
 earlyYears*= 10.5;
-//
+//the rest of human age
 let laterYears= myAge - 2;
+// converted to dog years
 laterYears*= 4;
+//test to see if the result is correct for the conversions
 console.log(earlyYears);
 console.log(laterYears);
 var myAgeInDogYears= earlyYears + laterYears;
-
+//
+var myName= 'Alessio'.toLowerCase();
+//
+console.log('My name is ' + myName + '. I am ' + myAge + ' years which is ' + myAgeInDogYears + ' years old in dog years.');
